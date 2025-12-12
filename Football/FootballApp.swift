1709aebffe50fe1865724d90021198642d9e71f2
Rupeshkaadyan <134736518+Rupeshkaadyan@users.noreply.github.com>
2025-12-13 03:35:36 +0530

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ethan on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
